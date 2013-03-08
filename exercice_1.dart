2033022e585734b9700890c98bde6998a1a94501
Alexandra Fortier <alex_fortier06@hotmail.com>
2013-03-08 09:57:43 -0500

    version 1

--- exercice_1.dart
+++ exercice_1.dart
@@ -5,16 +5,100 @@ import 'dart:math';
 part 'functions.dart'; 
 exercice_1_1()  {
   var placement = 10000;
-  var taux = 0.055;
-  var ataux = 1.055; 
-  var an = 10;
-  var r = interet (placement, ataux, an);
-  print (r);
+  var taux = 1.055;
+  var r =""; 
+  for (var i = 1; i<= 10; i++){
+  r = interet (placement, taux, i); 
+  print(r);
+  }
 }
+
+exercice_1_2() {
+ var x= 14;
+ var y = 0;
+ while (y <= 99) {
+   print(table(x, y));
+   y++;
+ }
+}
+
+exercice_1_3() {
+  print('    *');
+  print('   ***');
+  print('  *****');
+  print(' *******');
+  print('*********');
+}
+
+exercice_1_4() {
+  var a = 4; // prenons, par exemple, une valeur de 4 radians
+  ex_1_4();
+}
+
+exercice_1_5() {
+  var text = 'Nous mangeons des boulettes';
+  ex_1_5(text); 
+}
+
+exercice_1_6() {
+  var mot = 'laval';
+  ex_1_6(mot);
+}
+
+exercice_1_7() {
+  var prenoms = new Set();
+  prenoms.add('Jean');
+  prenoms.add('Maximilien');
+  prenoms.add('Brigitte');
+  prenoms.add('Sonia');
+  prenoms.add('Jean-Pierre');
+  prenoms.add('Sandra');
+  ex_1_7(prenoms);
+}
+
+exercice_1_8() {
+  var n = 75;
+  ex_1_8(n);
+}
+
+exercice_1_9() {
+  var prenomLongueur = {};
+  prenomLongueur['Jean'] = 4;
+  prenomLongueur['Maximilien'] = 10;
+  prenomLongueur['Brigitte'] = 8;
+  prenomLongueur['Sonia'] = 5;
+  prenomLongueur['Jean-Pierre'] = 11;
+  prenomLongueur['Sandra'] = 6;
+  ex_1_9(prenomLongueur);
+}
+
 
 void main() {
  print("Exercice 1");
- print('');
  exercice_1_1() ;
+ print('');
+ print("Exercice 2");
+ exercice_1_2();
+ print('');
+ print("Exercice 3");
+ exercice_1_3();
+ print('');
+ print("Exercice 4");
+ exercice_1_4();
+ print('');
+ print ("Exercice 5");
+ exercice_1_5();
+ print('');
+ print ("Exercice 6");
+ exercice_1_6();
+ print('');
+ print ("Exercice 7");
+ exercice_1_7();
+ print('');
+ print ("Exercice 8");
+ exercice_1_8();
+ print('');
+ print ("Exercice 9");
+ exercice_1_9();
 }
 
--- functions.dart
+++ functions.dart
@@ -1,5 +1,141 @@
 part of exercice_1;
 
-num interet(num a, num b, num c) {
+num interet(num a, var b, num c) {
   return a * pow(b, c);
 }
+
+num table(num x, num y){
+  return x * y;
+} 
+
+ex_1_4(){
+  var a = 4;
+  var degres = a *(180/PI);
+  var degresD = degres.toInt();
+  var decDegres1 = degres - (degres.floor());
+  var minutes = decDegres1 * 60;
+  var minutesM = minutes.toInt();
+  var decDegres2 = minutes-(minutes.floor());
+  var secondes = decDegres2*60;
+  var secondesS = secondes.toInt();
+  print ('${a} Radians équivaut à ${degresD} Degrés, ${minutesM} Minutes, ${secondesS} Secondes');
+}
+
+int letterFrequency(String text) {
+  String textWouthSpaces = text.replaceAll('\n', '').
+      replaceAll('a', ''). replaceAll('b', '').
+      replaceAll('c', '').replaceAll('d', '').
+      replaceAll('e', '').replaceAll('f', '').
+      replaceAll('g', '').replaceAll('h', '').
+      replaceAll('i', '').replaceAll('j', '').
+      replaceAll('k', '').replaceAll('l', '').
+      replaceAll('m', '').replaceAll('n', '').
+      replaceAll('o', 'o').replaceAll('p', '').
+      replaceAll('q', '').replaceAll('r', '').
+      replaceAll('s', '').replaceAll('t', '').
+      replaceAll('u', '').replaceAll('v', '').
+      replaceAll('w', '').replaceAll('x', '').
+      replaceAll('y', '').replaceAll('z', '').
+      replaceAll(' ', '').replaceAll('.', '').
+      replaceAll('/', '').replaceAll(' " ','').
+      replaceAll(' " ', '').replaceAll(',', '').
+      replaceAll('è', '').replaceAll('È', '').
+      replaceAll('ê', '').replaceAll('Ê', '').
+      replaceAll('é', '').replaceAll('É', '').
+      replaceAll('à', '').replaceAll('À', '').
+      replaceAll('ï', '').replaceAll('Ï', '').
+      replaceAll('ë', '').replaceAll('Ë', '').
+      replaceAll('ç', '').replaceAll('Ç', '');
+  List charList = textWouthSpaces.split('');
+  charList.sort((m,n) => m.compareTo(n));
+  var charMap = {};
+  for (var char in charList) {
+    if (char == 'o') {
+    charMap [char] = charMap.putIfAbsent(char, () => 0) +1; 
+    }
+  }
+  return charMap['o'];
+      }
+
+ex_1_5(var text) {
+  print ("Le nombre d'occurences du caractere o: ${letterFrequency(text)}");
+}
+
+
+String reverse(String s) {
+  StringBuffer sb=new StringBuffer();
+  for (int i=s.length-1;i>=0; i--) {
+    sb.write(s[i]);
+  }
+  return sb.toString();
+}
+
+ex_1_6(var mot) {
+  print('Le Mot: ${mot}');
+  print('Le mot renversé:${reverse(mot)}');
+  print('Est-ce un palindrome? --> ${mot == reverse(mot)}');
+}
+
+
+ex_1_7(var prenoms) {
+  var countunder6 = prenoms.where((p) => p.length < 6);
+  var countover6 = prenoms.where((p) => p.length >= 6);
+  print('Mots ayant moins de 6 lettre: ${countunder6}');
+  print('Mots ayant 6 lettres ou plus: ${countover6}');
+}
+
+ex_1_8(var n) {
+  if (n >= 90) {
+    print('La note est A');
+  }
+  if (n < 90) while (n >= 80) {
+    print('La note est B');
+    break;
+  }
+  if (n < 80) while (n >= 70) {
+    print('La note est C');
+    break;
+  }
+  if (n < 70) while (n >= 60) {
+    print('La note est D');
+    break;
+  }
+  if (n < 60) {
+    print('La note est E');
+  }
+}
+
+ex_1_9(var prenomLongueur) {
+  print('Les prénoms avec leur longueur: ${prenomLongueur}');
+}
+
+List fromTextToWords(String text) {
+  RegExp regexp = const RegExp("[,;:.?!()'`'/ \n]");
+  var textWithout = text.replaceAll(regexp,'');
+  return textWithout.split('  ');
+}
+
+Map analyseWordFrequency(List wordList) {
+  var wordFrequencyMap = new Map();
+  for (var w in wordList) {
+    var word = w.trim();
+    if (word != '') {
+      if (!wordFrequencyMap.containsKey(word)) {
+        wordFrequencyMap[word] = 0;
+      }
+      wordFrequencyMap[word] = wordFrequencyMap[word] +1;
+    }
+  }
+  return wordFrequencyMap;
+}
+
+List sortWords (Map wordFrequencyMap) {
+  var wordWordFrequencyMap = new Map<String, String>();
+  wordFrequencyMap.forEach((k, v) =>
+      wordWordFrequencyMap[k] = '${k}: ${v.toString()}');
+  List sortedWordList = wordWordFrequencyMap.values;
+  sortedWordList.sort((m, n) => m.compareTo(n));
+  return sortedWordList;
+}
+
+
